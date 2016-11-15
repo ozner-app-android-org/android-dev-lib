c4a23b5b84f6220753b237c4b4e8cb3ccb096d1b
xuzhiyong <xzyxdtxxx@gmail.com>
2016-11-15 15:45:49 +0800

    WIFI水机不能配网BUG

--- OznerLibrary/src/main/java/com/ozner/WaterPurifier/WaterPurifierManager.java
+++ OznerLibrary/src/main/java/com/ozner/WaterPurifier/WaterPurifierManager.java
@@ -47,7 +47,10 @@ public class WaterPurifierManager extends BaseDeviceManager {
         {
             return true;
         }
-
+        if (Model.trim().equals("16a21bd6"))
+        {
+            return true;
+        }
 
         return false;
 
